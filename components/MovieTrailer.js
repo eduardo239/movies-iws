1516abbdd4b66e3f1da89f62e2db8c4d51ebcc18
eduardo239 <eucrieiumaconta@gmail.com>
2021-10-16 17:05:19 -0300

    fix

--- components/MovieTrailer.js
+++ components/MovieTrailer.js
@@ -4,7 +4,7 @@ export default function MovieTrailer({ trailer_id }) {
       <iframe
         width="684"
         height="386"
-        src={`http://www.youtube.com/embed/${trailer_id}`}
+        src={`https://www.youtube.com/embed/${trailer_id}`}
         frameBorder="0"
         allowFullScreen
       ></iframe>
