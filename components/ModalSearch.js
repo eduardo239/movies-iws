b8e543962a5cb020bb1d2902a09cef864fe6772e
eduardo239 <eucrieiumaconta@gmail.com>
2021-10-24 09:54:28 -0300

    componente Search ajustado

--- components/ModalSearch.js
+++ components/ModalSearch.js
@@ -3,6 +3,8 @@ import ImageCardTrailer from './ImageCardTrailer';
 import Masonry from 'react-masonry-css';
 import { breakpointColumnsObj } from '../utils/constants';
 import closeIcon from '../assets/eva_close-outline.svg';
+import Link from 'next/link';
+import poster_default from '../assets/poster.png';
 
 export default function ModalSearch({ toggleModal, show, children, data }) {
   const showHideClassName = show ? 'modal-open' : 'modal-close';
@@ -25,16 +27,39 @@ export default function ModalSearch({ toggleModal, show, children, data }) {
             className="my-masonry-grid"
             columnClassName="my-masonry-grid_column"
           >
-            {data?.map((x) => (
-              <ImageCardTrailer key={x.id} content={x}></ImageCardTrailer>
+            {data?.slice(0, 9).map((content) => (
+              <div className="card-item" key={content.id}>
+                <Link href={`/movie/${content.id}`} passHref>
+                  <a>
+                    <div className="card-item__poster-div">
+                      <img
+                        className="card-item__poster"
+                        src={`${
+                          content.poster_path
+                            ? 'http://image.tmdb.org/t/p/w185' +
+                              content.poster_path
+                            : poster_default.src
+                        }`}
+                        alt={
+                          content.original_title
+                            ? content.original_title
+                            : content.original_name
+                            ? content.original_name
+                            : `none`
+                        }
+                      />
+                    </div>
+                  </a>
+                </Link>
+              </div>
             ))}
+            <div className="flex-center mb-20">
+              <button className="btn-icon btn-primary" onClick={toggleModal}>
+                <img src={closeIcon.src} alt="Trailer" />
+                close
+              </button>
+            </div>
           </Masonry>
-        </div>
-        <div className="flex-center mb-20">
-          <button className="btn-icon btn-primary" onClick={toggleModal}>
-            <img src={closeIcon.src} alt="Trailer" />
-            close
-          </button>
         </div>
       </section>
     </div>
--- components/Search.js
+++ components/Search.js
@@ -9,42 +9,43 @@ export default function Search() {
   const toggleModal = () => {
     setShowModal(!showModal);
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    searchMovies();
-  };
-  const searchMovies = async () => {
-    if (search.length > 2) {
+    if (search.length > 1) {
       const res = await fetch(
         `https://api.themoviedb.org/3/search/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_KEY}&language=pt-BR&query=${search}&page=1&include_adult=false`
       );
       const data = await res.json();
       setResults(data.results);
       setShowModal(true);
-
-      if (results.length > 0) {
-        setShowModal(true);
-      } else {
-        setShowModal(false);
-      }
     } else {
-      setResults([]);
       setShowModal(false);
     }
   };
 
+  const handleReset = () => {
+    setSearch('');
+    setShowModal(false);
+    setResults([]);
+  };
+
   return (
     <div className="search">
-      <form onSubmit={handleSubmit} onChange={searchMovies}>
-        <div className="form-group">
-          <label htmlFor="login-email">Busca</label>
-          <input
-            required
-            type="text"
-            id="search"
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
+      <form onChange={handleSubmit} className="flex-center">
+        <div style={{ width: '100%', display: 'flex', alignItems: 'center' }}>
+          <div className="form-group">
+            <label htmlFor="login-email">Busca</label>
+            <input
+              type="text"
+              id="search"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+          <button className="btn btn-primary" onClick={handleReset}>
+            x
+          </button>
         </div>
       </form>
       <div className="search-result">
